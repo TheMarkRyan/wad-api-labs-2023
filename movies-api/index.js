ac3c6dc918680c725d6599140b75ee6566b841fc
TheMarkRyan <20098241@mail.wit.ie>
2023-12-28 09:22:04 +0000

    Added Movies Route

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import moviesRouter from './api/movies';   //import movies router
 import usersRouter from './api/users';
 import './db';
 import defaultErrHandler from './errHandler'
@@ -13,6 +14,7 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', usersRouter);
+app.use('/api/movies', moviesRouter); 
 app.use(defaultErrHandler);
 
 app.listen(port, () => {
